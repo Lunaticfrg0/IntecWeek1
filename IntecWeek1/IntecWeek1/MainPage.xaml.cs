4b2549606065e8b00d18953f9495d7643fb749ef
Emilio Escaño <emiliojoseescano@hotmail.com>
2019-08-23 19:41:06 -0400

    Add project files.

---
+++ IntecWeek1/IntecWeek1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace IntecWeek1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        //void OpenBotton(object obj, EventArgs clicked)
+        //{
+        //    var button = (Button)obj;
+        //    cc.Text = aa.Text;
+        //    cc.TextColor = button.BackgroundColor;
+        //}
+       async void ColorChanger(object obj, EventArgs evento)
+        {
+            var button = (Button)obj;
+            Nombre.Text = Entrada.Text;
+            Nombre.TextColor = button.BackgroundColor;
+            await Navigation.PushAsync(new MyNewPage(button.BackgroundColor, button.Text));
+            
+        }
+        
+        
+    }
+}
---
+++ IntecWeek1/IntecWeek1/MyNewPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntecWeek1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyNewPage : ContentPage
+    {
+        public MyNewPage(Color color, string text )
+        {
+            InitializeComponent();
+            this.BackgroundColor = color;
+            LabelNew.Text = text;
+            
+        }
+
+    }
+}
